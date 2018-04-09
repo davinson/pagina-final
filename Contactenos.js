ca02283d277c6269e43871ec6b15ef3464ce1cea
davinson garcia <davinson_09@hotmail.com>
2018-04-08 23:05:14 -0400

    Add files via upload

---
+++ Contactenos.js
@@ -0,0 +1,289 @@
+var features = 'toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes,status=no,left=,top=,width=,height=';
+var searchDatabase = new SearchDatabase();
+var searchResults_length = 0;
+var searchResults = new Object();
+function searchPage(features)
+{
+   var element = document.getElementById('SiteSearch1');
+   if (element.value.length != 0 || element.value != " ")
+   {
+      var value = unescape(element.value);
+      var keywords = value.split(" ");
+      searchResults_length = 0;
+      for (var i=0; i<database_length; i++)
+      {
+         var matches = 0;
+         var words = searchDatabase[i].title + " " + searchDatabase[i].description + " " + searchDatabase[i].keywords;
+         for (var j = 0; j < keywords.length; j++)
+         {
+            var pattern = new RegExp(keywords[j], "i");
+            var result = words.search(pattern);
+            if (result >= 0)
+            {
+               matches++;
+            }
+            else
+            {
+               matches = 0;
+            }
+         }
+         if (matches == keywords.length)
+         {
+            searchResults[searchResults_length++] = searchDatabase[i];
+         }
+      }
+      var wndResults = window.open('about:blank', '', features);
+      setTimeout(function()
+      {
+         var results = '';
+         var html = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta http-equiv="Content-Language" content="es-ve"><title>Search results<\/title><\/head>';
+         html = html + '<body style="background-color:#FFFFFF;margin:0;padding:2px 2px 2px 2px;">';
+         html = html + '<span style="font-family:Arial;font-size:13px;color:#000000">';
+         for (var n=0; n<searchResults_length; n++)
+         {
+            var page_keywords = searchResults[n].keywords;
+            results = results + '<strong><a style="color:#0000FF" target="_parent" href="'+searchResults[n].url+'">'+searchResults[n].title +'<\/a><\/strong><br>Palabras claves: ' + page_keywords +'<br><br>\n';
+         }
+         if (searchResults_length == 0)
+         {
+            results = 'No hay resultados';
+         }
+         else
+         {
+            html = html + searchResults_length;
+            html = html + 'resultados de la busqueda:';
+            html = html + value;
+            html = html + '<br><br>';
+         }
+         html = html + results;
+         html = html + '<\/span>';
+         html = html + '<\/body><\/html>';
+         wndResults.document.write(html);
+     },100);
+   }
+   return false;
+}
+function searchParseURL()
+{
+   var url = decodeURIComponent(window.location.href);
+   if (url.indexOf('?') > 0)
+   {
+      var terms = '';
+      var params = url.split('?');
+      var values = params[1].split('&');
+      for (var i=0;i<values.length;i++)
+      {
+         var param = values[i].split('=');
+         if (param[0] == 'q')
+         {
+            terms = unescape(param[1]);
+            break;
+         }
+      }
+      if (terms != '')
+      {
+         var element = document.getElementById('SiteSearch1');
+         element.value = terms;
+         searchPage('');
+      }
+   }
+}
+function Validatecontacto(theForm)
+{
+   var regexp;
+   regexp = /^[A-Za-zÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ]*$/;
+   if (!regexp.test(theForm.Editbox1.value))
+   {
+      alert("Introduzca un nombre  valido por favor");
+      theForm.Editbox1.focus();
+      return false;
+   }
+   if (theForm.Editbox1.value == "")
+   {
+      alert("Introduzca un nombre  valido por favor");
+      theForm.Editbox1.focus();
+      return false;
+   }
+   if (theForm.Editbox1.value.length < 5)
+   {
+      alert("Introduzca un nombre  valido por favor");
+      theForm.Editbox1.focus();
+      return false;
+   }
+   if (theForm.Editbox1.value.length > 15)
+   {
+      alert("Introduzca un nombre  valido por favor");
+      theForm.Editbox1.focus();
+      return false;
+   }
+   if (theForm.Editbox2.value == "")
+   {
+      alert("erroe en apellido");
+      theForm.Editbox2.focus();
+      return false;
+   }
+   if (theForm.Editbox2.value.length < 6)
+   {
+      alert("erroe en apellido");
+      theForm.Editbox2.focus();
+      return false;
+   }
+   if (theForm.Editbox2.value.length > 15)
+   {
+      alert("erroe en apellido");
+      theForm.Editbox2.focus();
+      return false;
+   }
+   regexp = /^([0-9a-z]([-.\w]*[0-9a-z])*@(([0-9a-z])+([-\w]*[0-9a-z])*\.)+[a-z]{2,6})$/i;
+   if (theForm.Editbox3.value.length != 0 && !regexp.test(theForm.Editbox3.value))
+   {
+      alert("Introduzca una email valido");
+      theForm.Editbox3.focus();
+      return false;
+   }
+   regexp = /^[-+]?\d*\.?\d*$/;
+   if (!regexp.test(theForm.Editbox4.value))
+   {
+      alert("Introduzca una edad valida");
+      theForm.Editbox4.focus();
+      return false;
+   }
+   regexp = /^[-+]?\d*\.?\d*$/;
+   if (!regexp.test(theForm.Editbox7.value))
+   {
+      alert("Introduzca una cedula valida");
+      theForm.Editbox7.focus();
+      return false;
+   }
+   if (theForm.Editbox7.value == "")
+   {
+      alert("Introduzca una cedula valida");
+      theForm.Editbox7.focus();
+      return false;
+   }
+   if (theForm.Editbox7.value.length < 8)
+   {
+      alert("Introduzca una cedula valida");
+      theForm.Editbox7.focus();
+      return false;
+   }
+   if (theForm.Editbox7.value.length > 8)
+   {
+      alert("Introduzca una cedula valida");
+      theForm.Editbox7.focus();
+      return false;
+   }
+   return true;
+}
+$(document).ready(function()
+{
+   searchParseURL();
+   var $autocomplete = $('<ul class="autocomplete"></ul>').hide().insertAfter('#SiteSearch1');
+   var selectedItem = null;
+   var setSelectedItem = function(item)
+   {
+      selectedItem = item;
+      if (selectedItem === null)
+      {
+         $autocomplete.hide();
+         return;
+      }
+      if (selectedItem < 0)
+      {
+         selectedItem = 0;
+      }
+      if (selectedItem >= $autocomplete.find('li').length)
+      {
+         selectedItem = $autocomplete.find('li').length - 1;
+      }
+      $autocomplete.find('li').removeClass('selected').eq(selectedItem).addClass('selected');
+      $autocomplete.css('left', $('#SiteSearch1').position().left);
+      $autocomplete.css('top', $('#SiteSearch1').position().top + $('#SiteSearch1').outerHeight());
+      $autocomplete.show();
+   };
+   var populateSearchField = function()
+   {
+      $('#SiteSearch1').val($autocomplete.find('li').eq(selectedItem).text());
+      setSelectedItem(null);
+   };
+   $('#SiteSearch1').attr('autocomplete', 'off').keyup(function(event)
+   {
+      if (event.keyCode > 40 || event.keyCode == 8)
+      {
+         var data = new Array();
+         var keywordVal = $('#SiteSearch1').val();
+         keywordVal = keywordVal.toLowerCase();
+         for (i=0; i<database_length; i++)
+         {
+            var words = (searchDatabase[i].title + " " + searchDatabase[i].description + " " + searchDatabase[i].keywords).toLowerCase();
+            var array = words.split(" ");
+            data = $.merge(data, array);
+         }
+
+         var unique = new Array();
+         o:for(var i = 0; i < data.length; i++)
+         {
+            for(var j = 0; j < unique.length; j++)
+            {
+               if(unique[j]==data[i]) continue o;
+            }
+            unique[unique.length] = data[i];
+         }
+
+         unique.sort();
+         if (keywordVal.length && unique.length)
+         {
+            $autocomplete.empty();
+            var item = 0;
+            $.each(unique, function(index, term)
+            {
+               term = term.toLowerCase();
+               if (term.indexOf(keywordVal) == 0)
+               {
+                  $('<li></li>').text(term).data('item', item).appendTo($autocomplete).mouseover(function()
+                  {
+                     var item = $(this).data('item');
+                     setSelectedItem(item);
+                  }).click(populateSearchField);
+                  item++;
+               }
+            });
+            setSelectedItem(0);
+         }
+         else
+         {
+            setSelectedItem(null);
+         }
+      }
+      else
+      if (event.keyCode == 38 && selectedItem !== null)
+      {
+         setSelectedItem(selectedItem - 1);
+         event.preventDefault();
+      }
+      else
+      if (event.keyCode == 40 && selectedItem !== null)
+      {
+         setSelectedItem(selectedItem + 1);
+         event.preventDefault();
+      }
+      else
+      if (event.keyCode == 27 && selectedItem !== null)
+      {
+         setSelectedItem(null);
+      }
+   }).keypress(function(event)
+   {
+      if (event.keyCode == 13 && selectedItem !== null)
+      {
+         populateSearchField();
+         event.preventDefault();
+      }
+   }).blur(function(event)
+   {
+      setTimeout(function()
+      {
+         setSelectedItem(null);
+      }, 250);
+   });
+});
